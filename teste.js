cd9f15fef1ed2c82f05eecd6e115fd58862aaac2
Rafael Dos Santos <raphael_979@hotmail.com>
2019-12-22 16:12:54 -0300

    uso de Split para dividir string,transformando também palavras em letra Maiuscula.

---
+++ teste.js
@@ -0,0 +1,9 @@
+let umaString = 'O rato roeu a roupa do dei de roma';
+let umaString1 = 'O rato roeu a roupa do dei de roma';
+
+
+console.log(umaString.indexOf('rato'));
+console.log(umaString1.split('r')); //vai dividir no r
+console.log(umaString1.toUpperCase());//vai colocar a  palavra toda em maiúsculo
+console.log(umaString1.toLocaleLowerCase());
+
